aeadb939ec81a62586feda1704fe0a6747702752
Cerambyx Cerdo <cerambyxtasy@gmail.com>
2013-09-21 18:39:02 +0200

    Test commit

--- path/src/Cerambyxtasy/Oltree/MainBundle/Resources/public/js/hexagrid/base/HexagonTools.js
+++ path/src/Cerambyxtasy/Oltree/MainBundle/Resources/public/js/hexagrid/base/HexagonTools.js
@@ -25,7 +25,7 @@ HT.Rectangle = function(x, y, width, height) {
  */
 HT.Line = function(x1, y1, x2, y2) {
     this.X1 = x1;
-    this.Y1 = y1;
+    this.Y1 = y1; 
     this.X2 = x2;
     this.Y2 = y2;
 };
